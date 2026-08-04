2bfdd02870df1023ba1ccd3d5657b33bebad2631
EeshanArihant24 <eeshanarihant@gmail.com>
2026-08-04 23:22:21 +0530

    Added database backup files

---
+++ database/banking_db/banking_db_backup.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict zAzwSnO8M1P3C6fL34q7MbMZtqfZwPmRSOQMWq7Blm3svpWKvgOwdc34O4Vu5Ne
+
+-- Dumped from database version 14.23 (Ubuntu 14.23-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.23 (Ubuntu 14.23-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict zAzwSnO8M1P3C6fL34q7MbMZtqfZwPmRSOQMWq7Blm3svpWKvgOwdc34O4Vu5Ne
+
