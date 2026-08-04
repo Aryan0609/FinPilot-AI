31e7a9c1856011d922588bb5975e5f81837cb673
EeshanArihant24 <eeshanarihant@gmail.com>
2026-08-05 01:15:59 +0530

    Fixed local profile database configuration and application context test

--- banking-service/src/test/java/com/finpilot/banking/BankingServiceApplicationTests.java
+++ banking-service/src/test/java/com/finpilot/banking/BankingServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.finpilot.banking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class BankingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
