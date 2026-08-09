048b446813c03b562e061266de0563d17b0c8113
Aryan0609 <mankararyna0609@gmail.com>
2026-08-09 17:48:52 +0530

    testing frontend fix approutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,83 +1,45 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Login from "../pages/login/Login";
 import Register from "../pages/login/Register";
 
 import Dashboard from "../pages/Dashboard/Dashboard";
-
-import Wallet from "../pages/Banking/Wallet";
-import Deposit from "../pages/Banking/Deposit";
-import Withdraw from "../pages/Banking/Withdraw";
-import Transfer from "../pages/Banking/Transfer";
 import Transactions from "../pages/Banking/Transactions";
-import FD from "../pages/Banking/FD";
 import MutualFunds from "../pages/Banking/MutualFunds";
-
+import FixedDeposits from "../pages/Banking/FixedDeposits";
 import FraudDetection from "../pages/AI/FraudDetection";
-import Profile from "../pages/Profile/Profile";
 
-import PrivateRoute from "./PrivateRoute";
-import AdminRoute from "./AdminRoute";
-import AdminRoutes from "./AdminRoutes";
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import AdminTransactions from "../pages/Admin/Transactions";
+import AdminFraudDetection from "../pages/Admin/FraudDetection";
 
 export default function AppRoutes() {
   return (
     <Routes>
-
+      {/* Authentication */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
+      {/* User */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/transactions" element={<Transactions />} />
+      <Route path="/mutual-funds" element={<MutualFunds />} />
+      <Route path="/fixed-deposits" element={<FixedDeposits />} />
+      <Route path="/fraud-detection" element={<FraudDetection />} />
+
+      {/* Admin */}
+      <Route path="/admin" element={<AdminDashboard />} />
       <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
+        path="/admin/transactions"
+        element={<AdminTransactions />}
+      />
+      <Route
+        path="/admin/fraud-detection"
+        element={<AdminFraudDetection />}
       />
 
-      <Route
-        path="/wallet"
-        element={
-          <PrivateRoute>
-            <Wallet />
-          </PrivateRoute>
-        }
-      />
-
-      <Route
-        path="/deposit"
-        element={
-          <PrivateRoute>
-            <Deposit />
-          </PrivateRoute>
-        }
-      />
-
-      <Route
-        path="/withdraw"
-        element={
-          <PrivateRoute>
-            <Withdraw />
-          </PrivateRoute>
-        }
-      />
-
-      <Route
-        path="/transfer"
-        element={
-          <PrivateRoute>
-            <Transfer />
-          </PrivateRoute>
-        }
-      />
-
-      <Route
-        path="/transactions"
-        element={
-          <PrivateRoute>
-            <Transactions />
-          </PrivateRoute>
-        }
-      />
-
+      {/* Default */}
+      <Route path="*" element={<Login />} />
+    </Routes>
+  );
+}
